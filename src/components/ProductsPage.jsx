c27886913a757920e0fb8dfc4286adcff78084d7
munisamee <munisa.ermatova@gmail.com>
2024-02-17 17:01:14 +0500

    lab is done

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,38 +1,17 @@
   // src/components/ProductsPage.jsx
 
-  import { useEffect, useState } from "react";
+  import { useState } from "react";
   import jsonData from "../data.json";
+  import ProductTable from "./ProductTable";
 
   function ProductsPage() {
-    const [products, setProducts] = useState([jsonData]);
+    const [products, setProducts] = useState(jsonData);
 
-    useEffect(() => {
-      // fetching data from JSON
-      const fetchData = async () => {
-        try {
-          const response = await response.json('data.json');
-          const data = await response.json();
-          setProducts(data);
-        } catch (error) {
-          console.log('Error fetching data:', error);
-        }
-      };
-
-      fetchData();
-    }, []);
-    
 
     return (
       <div>
         <h1>IronStore</h1>
-        {products.map(product => (
-        <div key={product.id}>
-          <h1>{product.category}</h1>
-          <p>Name: {product.name}</p>
-          <p>Price: {product.price}</p>
-          <p>In Stock: {product.inStock ? 'Yes' : 'No'}</p>
-        </div>
-      ))}
+        <ProductTable products={products} />
       </div>
       );
     }
